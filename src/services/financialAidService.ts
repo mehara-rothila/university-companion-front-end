b7210f65f5a269f93be2237ec53dbb995d3f957a
mehara-rothila <rothilamehara22@gmail.com>
2026-01-25 18:59:55 +0530

    Update financial aid service

--- src/services/financialAidService.ts
+++ src/services/financialAidService.ts
@@ -116,9 +116,11 @@ class FinancialAidService {
 
   constructor() {
     this.api.interceptors.request.use((config) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('token');
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
       }
       return config;
     });
